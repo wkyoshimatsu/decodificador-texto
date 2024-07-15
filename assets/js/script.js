311c4a51738d9be4c9b7817e06b90db6bb70ae1c
wkyoshimatsu <101765683+wkyoshimatsu@users.noreply.github.com>
2024-07-15 09:50:20 -0300

    Modularizar ítens principais na página principal

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,18 +7,30 @@ document.addEventListener('DOMContentLoaded', function() {
       .then(data => {
         document.getElementById('cabecalho').innerHTML = data;
       });
+
+      fetch('/components/entrada.html')
+      .then(response => response.text())
+      .then(data => {
+        document.getElementById('entrada').innerHTML = data;
+      });
+
+      fetch('/components/saida.html')
+      .then(response => response.text())
+      .then(data => {
+        document.getElementById('saida').innerHTML = data;
+      });
+
+      fetch('/components/nenhumaMensagemEncontrada.html')
+    .then(response => response.text())
+    .then(data => {
+      document.getElementById('apresentacao__saida__conteudo').innerHTML = data;
+    });
   
     fetch('/components/footer.html')
       .then(response => response.text())
       .then(data => {
         document.getElementById('rodape').innerHTML = data;
       });
-
-    fetch('/components/nenhumaMensagemEncontrada.html')
-    .then(response => response.text())
-    .then(data => {
-      document.getElementById('apresentacao__saida__conteudo').innerHTML = data;
-    });
 });
   
   
