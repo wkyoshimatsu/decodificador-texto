846004b3b580b33a62d296d26f20341c8360cd5c
wkyoshimatsu <101765683+wkyoshimatsu@users.noreply.github.com>
2024-07-16 13:25:50 -0300

    Alterado estrutura do projeto

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,46 +1,4 @@
-document.addEventListener('DOMContentLoaded', function() {
-    console.log('Documento carregado!');
-    
-    // Incluir conteúdos iniciais dinamicamente
-    fetch('/components/header.html')
-      .then(response => response.text())
-      .then(data => {
-        document.getElementById('cabecalho').innerHTML = data;
-      });
-
-    fetch('/components/entrada.html')
-      .then(response => response.text())
-      .then(data => {
-        document.getElementById('entrada').innerHTML = data;
-      });
-
-    fetch('/components/saida.html')
-      .then(response => response.text())
-      .then(data => {
-        document.getElementById('saida').innerHTML = data;
-      });
-
-    fetch('/components/saidaCriptografada.html')
-      .then(response => response.text())
-      .then(data => {
-        document.getElementById('saida__conteudo').innerHTML = data;
-      });
-  
-    fetch('/components/footer.html')
-      .then(response => response.text())
-      .then(data => {
-        document.getElementById('rodape').innerHTML = data;
-      });
-});
-
 function criptografar () {
-  /*
-  A letra "e" é convertida para "enter"
-  A letra "i" é convertida para "imes"
-  A letra "a" é convertida para "ai"
-  A letra "o" é convertida para "ober"
-  A letra "u" é convertida para "ufat"
-  */
   let textoBase = document.querySelector("textarea").value;
   let textoEmProcessamento = textoBase.replaceAll("e", "enter");
   textoEmProcessamento = textoEmProcessamento.replaceAll("i", "imes");
