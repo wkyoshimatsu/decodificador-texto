30561c19bf65f1ff94846cd45fd364eed452b210
wkyoshimatsu <101765683+wkyoshimatsu@users.noreply.github.com>
2024-07-16 12:30:08 -0300

    Criado função criptografar

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,30 +1,30 @@
 document.addEventListener('DOMContentLoaded', function() {
     console.log('Documento carregado!');
     
-    // Incluir header e footer dinamicamente
+    // Incluir conteúdos iniciais dinamicamente
     fetch('/components/header.html')
       .then(response => response.text())
       .then(data => {
         document.getElementById('cabecalho').innerHTML = data;
       });
 
-      fetch('/components/entrada.html')
+    fetch('/components/entrada.html')
       .then(response => response.text())
       .then(data => {
         document.getElementById('entrada').innerHTML = data;
       });
 
-      fetch('/components/saida.html')
+    fetch('/components/saida.html')
       .then(response => response.text())
       .then(data => {
         document.getElementById('saida').innerHTML = data;
       });
 
-      fetch('/components/saidaCriptografada.html')
-    .then(response => response.text())
-    .then(data => {
-      document.getElementById('saida__conteudo').innerHTML = data;
-    });
+    fetch('/components/saidaCriptografada.html')
+      .then(response => response.text())
+      .then(data => {
+        document.getElementById('saida__conteudo').innerHTML = data;
+      });
   
     fetch('/components/footer.html')
       .then(response => response.text())
@@ -32,5 +32,17 @@ document.addEventListener('DOMContentLoaded', function() {
         document.getElementById('rodape').innerHTML = data;
       });
 });
+
+function criptografar () {
+  /*
+  A letra "e" é convertida para "enter"
+  A letra "i" é convertida para "imes"
+  A letra "a" é convertida para "ai"
+  A letra "o" é convertida para "ober"
+  A letra "u" é convertida para "ufat"
+  */
+  let textoBase = document.querySelector("textarea").value;
+  console.log(textoBase);
+}
   
   
