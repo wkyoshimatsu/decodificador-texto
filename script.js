5430d7b4861dc9e0c1f8fc05ac7556d1744b9788
wkyoshimatsu <101765683+wkyoshimatsu@users.noreply.github.com>
2024-07-17 14:31:53 -0300

    Refatorado método criptografar Adicionado tratamento de caracter inválido Adicionado tratamento de entrada vazia ou nula Adicionado chaves de criptografia e descriptografia

--- script.js
+++ script.js
@@ -1,38 +1,59 @@
+let chave_criptografia = [
+  {chave: "e", valor: "enter"},
+  {chave: "i", valor: "imes"},
+  {chave: "a", valor: "ai"},
+  {chave: "o", valor: "ober"},
+  {chave: "u", valor: "ufat"},
+]
+
+let chave_descriptografia = []
+for (let i in chave_criptografia){
+  let j = {
+    chave: chave_criptografia[i].valor,
+    valor: chave_criptografia[i].chave
+  }
+  chave_descriptografia.push(j);
+}
+
+let padrao_caracteres = /[^a-z]/gm;
+
 function exibirMensagemInicial(){
   document.getElementById("nenhumaMensagemEncontrada").style.display = "flex";
   document.getElementById("resultado").style.display = "none";
+
+  console.log(chave_criptografia);
+  console.log(chave_descriptografia);
 }
 
-function criptografar () {
-  let textoParaCriptografia = document.getElementById("entrada").value;
-  document.getElementById("entrada").value = "";
+function criptografar (tipo) {
+  let entrada = document.getElementById("entrada").value;
+  if (entrada == null || entrada == ""){
+    exibirMensagemInicial();
+    alert("Nenhuma mensagem encontrada\nDigite um texto que você deseja criptografar ou descriptografar.");
+  } else if (padrao_caracteres.test(entrada)){
+    exibirMensagemInicial();
+    alert("Apenas letras minúsculas e sem acento.");
+  } else {
+    let textoEmProcessamento = document.getElementById("entrada").value;
   
-  let textoEmProcessamento = textoParaCriptografia.replaceAll("e", "enter");
-  textoEmProcessamento = textoEmProcessamento.replaceAll("i", "imes");
-  textoEmProcessamento = textoEmProcessamento.replaceAll("a", "ai");
-  textoEmProcessamento = textoEmProcessamento.replaceAll("o", "ober");
-  let textoCriptografado = textoEmProcessamento.replaceAll("u", "ufat");
-
-  document.getElementById("nenhumaMensagemEncontrada").style.display = "none";
-  document.getElementById("resultado").style.display = "flex";
-
-  let campo = document.getElementById("resultado__conteudo");
-  campo.innerHTML = textoCriptografado;
-}
-
-function descriptografar () {
-  let textoParaDescriptografar = document.getElementById("entrada").value;
-  let textoEmProcessamento = textoParaDescriptografar.replaceAll("enter", "e");
-  textoEmProcessamento = textoEmProcessamento.replaceAll("imes", "i");
-  textoEmProcessamento = textoEmProcessamento.replaceAll("ai", "a");
-  textoEmProcessamento = textoEmProcessamento.replaceAll("ober", "o");
-  let textoDescriptografado = textoEmProcessamento.replaceAll("ufat", "u");
-
-  document.getElementById("nenhumaMensagemEncontrada").style.display = "none";
-  document.getElementById("resultado").style.display = "flex";
-
-  let campo = document.getElementById("resultado__conteudo");
-  campo.innerHTML = textoDescriptografado;
+    let chave;
+    if (tipo == 1){
+      chave = chave_criptografia;
+    } else {
+      chave = chave_descriptografia;
+    }
+    
+    for (let i in chave) {
+      textoEmProcessamento = textoEmProcessamento.replaceAll(chave[i].chave, chave[i].valor)
+    }
+    
+    document.getElementById("nenhumaMensagemEncontrada").style.display = "none";
+    document.getElementById("resultado").style.display = "flex";
+  
+    let campo = document.getElementById("resultado__conteudo");
+    campo.innerHTML = textoEmProcessamento;
+  }
+  document.getElementById("entrada").value = "";
 }
 
 function copiar() {
